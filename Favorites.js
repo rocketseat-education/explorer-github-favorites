eec070f02b4e93bfdb90337d377f3983eb572d98
Ana Silveira <ana.ccord@gmail.com>
2023-07-19 13:52:18 -0300

    Imutabilidade para trabalhar com os dados da aplicação

--- Favorites.js
+++ Favorites.js
@@ -23,8 +23,12 @@ export class Favorites {
 
 
     }
-
-
+    
+    delete(user) {
+        const filteredENtries = this.entries.filter(entry => entry.login !== user.login)
+        
+        console.log(filteredENtries)
+    }
 
 
 
@@ -63,6 +67,14 @@ export class FavoritesView extends Favorites {
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
 
             this.tbody.append(row)
         })
