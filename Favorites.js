f412e33e909ba827226db2710bdf5c8b05c76daf
Ana Silveira <ana.ccord@gmail.com>
2023-08-16 13:45:36 -0300

    Ajustes finais, revisão e próximos passos

--- Favorites.js
+++ Favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-    static search(username) {
-        const endpoint = `https://api.github.com/users/${username}`
-
-        return fetch(endpoint)
-        .then(data => data.json())
-        .then(({login, name, public_repos, followers}) => ({
-            login,
-            name,
-            public_repos,
-            followers
-        }))
-    }
-}
+import { GithubUser } from "./GithubUser.js"
 
 
 // classe que vai conter a lógica dos dados
@@ -41,6 +28,15 @@ export class Favorites {
 
     async add(username) {
         try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists){
+                throw new Error('Usuário já cadastrado')
+            }
+
+
+
             const user = await GithubUser.search(username)
 
             if(user.login === undefined) {
@@ -108,6 +104,7 @@ export class FavoritesView extends Favorites {
             
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
